5d2af05dfb89755cd457c5415212184250608f8b
mahi11web <maddelamahesh948@gmail.com>
2025-07-18 02:20:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Java Full Stack Developer", "Web Developer", "entrepreneur", "Innovative Thinker"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true
+});
